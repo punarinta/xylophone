5032dcf1fa0be5f0249008832e8267c35686ceac
Vladimir Osipov <vladimir.osipov@coursio.com>
2021-05-21 23:03:20 +0300

    notes input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 My daughter got a xylophone as a toy, but she's too little to play it yet. 
 However, I'd like to do that instead, and this app is here to assist me. :)
+
+---
+
+Work in progress
--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,38 @@
+import { useCallback, useState } from 'react'
 import KeySet from '../KeySet'
 import './index.css'
 
 export default function App () {
+  const [notes, setNotes] = useState([])
+
+  const onNotesChange = useCallback((v) => {
+    const text = v.target.value.trim()
+    const notes = text.split(' ').map(pair => pair.split('/'))
+
+    setNotes(notes)
+  }, [])
+
+  const startPlay = useCallback(() => {
+    console.log(notes)
+  }, [notes])
+
   return (
     <div className="app">
+      <div className="app__notes">
+        <textarea
+          autoFocus={true}
+          className="app__notes-input"
+          cols="30"
+          rows="4"
+          onChange={onNotesChange}
+        />
+        <button
+          className="app__notes-play"
+          onClick={startPlay}
+        >
+          Play
+        </button>
+      </div>
       <KeySet />
     </div>
   )
